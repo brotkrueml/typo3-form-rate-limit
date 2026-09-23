a2e9d10f79efaed17efd47060208a0bfd34438bd
Chris Müller <coding@brotkrueml.dev>
2026-09-23 17:52:51 +0200

    Remove header comment from documentation's PHP files

--- Documentation/Events/_FormRateLimitExceededLogger.php
+++ Documentation/Events/_FormRateLimitExceededLogger.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the "form_rate_limit" extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- */
-
 namespace YourVendor\YourExtension\EventListener;
 
 use Brotkrueml\FormRateLimit\Event\RateLimitExceededEvent;
